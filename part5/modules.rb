08e6e7f7e5763d68925339994ed7f27422c57c6a
root <root@cubic.local>
2025-04-07 11:17:52 +0300

    Вынес меню в отдельный модуль в main

--- part5/modules.rb
+++ part5/modules.rb
@@ -27,32 +27,3 @@ module InstanceCounter
       end
     end
   end
-
-  module Menu
-    MENU = [
-      { id: 1, title: 'Создать станцию', action: :create_station },
-      { id: 2, title: 'Создать поезд', action: :create_train },
-      { id: 3, title: 'Создать маршрут', action: :create_route }, 
-      { id: 4, title: 'Назначить маршрут поезду', action: :assign_route }, 
-      { id: 5, title: 'Добавить вагон к поезду', action: :add_carriage }, 
-      { id: 6, title: 'Отцепить вагон от поезда', action: :remove_carriage }, 
-      { id: 7, title: 'Переместить поезд вперед', action: :move_train_forward }, 
-      { id: 8, title: 'Переместить поезд назад', action: :move_train_backward }, 
-      { id: 9, title: 'Просмотреть список станций и поездов на станциях', action: :list_stations_trains }, 
-      { id: 0, title: 'Выйти', action: :exit_app } 
-    ]
-  
-    def show_menu
-      MENU.each { |item| puts "#{item[:id]}. #{item[:title]}" }
-    end
-  
-    def get_choice
-      puts 'Выберите действие: '
-      gets.chomp.to_i
-    end
-  
-    def take_action(choice)
-      item = MENU.find { |menu_item| menu_item[:id] == choice }
-      item ? send(item[:action]) : puts('Некорректный ввод, попробуйте снова.')
-    end
-  end
